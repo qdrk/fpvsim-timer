a99e76a4d475a3c8da568249a8931474444665ef
qdrk <mirikle.wang@gmail.com>
2022-12-26 22:32:48 -0800

    Initial commit

---
+++ src/fpvsim_timer.cpp
@@ -0,0 +1,403 @@
+#include "fpvsim_timer.h"
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+void commitEeprom() {
+#ifdef DEV_MODE
+  Serial.println("Write EEPROM.");
+#endif
+
+  EEPROM.put(0, settings);
+  EEPROM.commit();
+}
+
+volatile bool settingsUpdated = false;
+volatile uint32_t lastSettingsUpdateTime = 0;
+
+void updateRssiTrigger() {
+  state.enterRssiTrigger =
+      settings.rssiPeak * (1.0 - settings.enterRssiOffset / 100.0);
+  state.leaveRssiTrigger =
+      settings.rssiPeak * (1.0 - settings.leaveRssiOffset / 100.0);
+
+#ifdef DEV_MODE
+  // NOTE: have to use %d instead of %s here to avoid crashing.
+  Serial.printf_P("peak: %d, enter: %d, leave: %d \n", settings.rssiPeak,
+                  state.enterRssiTrigger, state.leaveRssiTrigger);
+#endif
+
+  settingsUpdated = true;
+}
+
+char apSsid[11];
+void setApSsid() {
+  strcpy(apSsid, "quadrank- \0");
+  apSsid[9] = char('a' + settings.id);
+}
+
+void setupServer() {
+  setApSsid();
+
+  // Begin Access Point
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(apSsid);
+
+  // Begin WiFi
+  if (strlen(settings.routerSsid) > 0) {
+    WiFi.begin(settings.routerSsid, settings.routerPwd);
+
+    // Connecting to WiFi...
+    Serial.print("Connecting to ");
+    Serial.print(settings.routerSsid);
+    int counter = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      counter += 1;
+      delay(100);
+      Serial.print(".");
+      if (counter > 150) {
+        Serial.println("Failed to connect to router. Skipping.");
+        break;
+      }
+    }
+  }
+
+  // Connected to WiFi
+  Serial.println();
+  Serial.println("Connected!");
+  Serial.print("IP address for network ");
+  Serial.print(settings.routerSsid);
+  Serial.print(" : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("IP address for network ");
+  Serial.print(apSsid);
+  Serial.print(" : ");
+  Serial.print(WiFi.softAPIP());
+  Serial.println("");
+
+  // Dump network settings.
+  strcpy(settings.apSsid, apSsid);
+  strcpy(settings.localIp, WiFi.localIP().toString().c_str());
+  strcpy(settings.apIp, WiFi.softAPIP().toString().c_str());
+
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Private-Network", "*");
+
+  // Settings.
+  server.on("/api/v1/settings", HTTP_GET, [](AsyncWebServerRequest *request) {
+    state.clientConnected = true;
+
+    request->send(200, "text/json", settingsToJson().c_str());
+  });
+
+  updateRssiTrigger();
+
+  server.on("/api/v1/settings", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if (!request->hasParam("rssiPeak") ||
+        !request->hasParam("enterRssiOffset") ||
+        !request->hasParam("leaveRssiOffset")) {
+      request->send(400, "text/plain", "Invalid params");
+      return;
+    }
+
+    settings.rssiPeak =
+        std::atoi(request->getParam("rssiPeak")->value().c_str());
+    settings.enterRssiOffset =
+        std::atoi(request->getParam("enterRssiOffset")->value().c_str());
+    settings.leaveRssiOffset =
+        std::atoi(request->getParam("leaveRssiOffset")->value().c_str());
+
+    updateRssiTrigger();
+    commitEeprom();
+
+    Serial.print("Updated settings:");
+    Serial.println(settingsToJson().c_str());
+
+    request->send(200, "text/json", settingsToJson().c_str());
+  });
+
+  server.on("/api/v1/wifisettings", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if (!request->hasParam("routerSsid") ||
+        !request->hasParam("routerPwd")) {
+      request->send(400, "text/plain", "Invalid params");
+      return;
+    }
+
+    strcpy(settings.routerSsid, request->getParam("routerSsid")->value().c_str());
+    strcpy(settings.routerPwd, request->getParam("routerPwd")->value().c_str());
+
+    commitEeprom();
+
+    Serial.print("Updated settings:");
+    Serial.println(settingsToJson().c_str());
+
+    request->send(200, "text/json", settingsToJson().c_str());
+
+    // Restart the server.
+    ESP.restart();
+  });
+
+
+  // Calibration.
+  // Somehow, PUT fails with CORS, POST works.
+  server.on("/api/v1/start", HTTP_POST, [](AsyncWebServerRequest *request) {
+    settings.rssiPeak = 0;
+    state.calibrationMode = true;
+    state.calibrationPasses = 0;
+    state.calibrationStartMicros = micros();
+
+    Serial.println(">>>> Start calibration");
+
+    events.send("started", "calibration", state.calibrationStartMicros);
+    request->send(200, "text/json", settingsToJson().c_str());
+  });
+
+
+  // Frequency.
+  server.on("/api/v1/setFrequency", HTTP_POST,
+            [](AsyncWebServerRequest *request) {
+              if (!request->hasParam("frequency")) {
+                request->send(400, "text/plain", "No frequency");
+                return;
+              }
+
+              AsyncWebParameter *p = request->getParam("frequency");
+              int frequency = std::atoi(p->value().c_str());
+
+              state.newVtxFreq = frequency;
+
+              request->send(200, "text/json", settingsToJson().c_str());
+            });
+
+  // Setup events.
+  events.onConnect([](AsyncEventSourceClient *client) {
+    if (client->lastId()) {
+      Serial.printf("Client reconnected! Last message ID that it gat is: %u\n",
+                    client->lastId());
+    }
+    // Send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second.
+    client->send("hello!", NULL, millis(), 1000);
+  });
+  server.addHandler(&events);
+
+  server.begin();
+
+  Serial.println("HTTP server started");
+}
+
+
+void setup() {
+  Serial.begin(115200); // Start serial for output/debugging
+  Serial.println();
+  Serial.println();
+
+  // commit 256 bytes of ESP8266 flash (for "EEPROM" emulation)
+  // this step actually loads the content (256 bytes) of flash into
+  // a 256-byte-array cache in RAM
+  EEPROM.begin(256);
+
+  // read bytes (i.e. sizeof(data) from "EEPROM"),
+  // in reality, reads from byte-array cache
+  // cast bytes into structure called data
+  SettingsType settingsPref;
+  EEPROM.get(0, settingsPref);
+
+  // A heuristics to check EEPROM has been set, since this var is never changed.
+  if (settingsPref.filterRatio == 10) {
+    Serial.println("Loading from EEPROM.");
+    settings = settingsPref;
+    // So we don't accidentally reset the vtx freq.
+    state.newVtxFreq = settings.vtxFreq;
+
+    Serial.print("VTX: ");
+    Serial.println(settings.vtxFreq);
+
+    Serial.print("Rssi peak: ");
+    Serial.println(settings.rssiPeak);
+  } else {
+    Serial.println("EEPROM not set.");
+  }
+
+  // If no existing id, generate one.
+  if (settings.id < 0 || settings.id > 25) {
+    // Get a number from 0 to 25.
+    settings.id = random(26);
+    commitEeprom();
+  }
+
+  Serial.print("Timer id: ");
+  Serial.println(settings.id);
+
+  // RX5808 comms.
+  // SPI.begin();
+  pinMode(slaveSelectPin, OUTPUT);
+  pinMode(spiDataPin, OUTPUT);
+  pinMode(spiClockPin, OUTPUT);
+
+  digitalWrite(slaveSelectPin, HIGH);
+
+  while (!Serial) {
+  }; // Wait for the Serial port to initialise
+  Serial.println("Serial ready...");
+
+  // Initialize lastPass defaults
+  state.filterRatioFloat = settings.filterRatio / 1000.0f;
+
+  setRxModule(settings.vtxFreq);
+
+  setupServer();
+}
+
+uint32_t lastRssiSendTime = 0;
+// #if defined(ESP8266)
+const uint32_t rssiSendInterval = 3000 * 1000;
+// #else
+// // 500ms per rssi.
+// const uint32_t rssiSendInterval = 3000 * 1000;
+// #endif
+
+void loop() {
+  // If no client has connected, no need to loop.
+  if (!state.clientConnected)
+    return;
+
+  if (state.newVtxFreq != settings.vtxFreq) {
+    setRxModule(state.newVtxFreq);
+
+    commitEeprom();
+  }
+
+  // Calculate the time it takes to run the main loop
+  uint32_t previousLoopTimestamp = state.lastLoopTimeStamp;
+  state.lastLoopTimeStamp = micros();
+  state.loopTime = state.lastLoopTimeStamp - previousLoopTimestamp;
+
+  // Two settings update has to be larger than 1s interval.
+  if (settingsUpdated &&
+      state.lastLoopTimeStamp - lastSettingsUpdateTime > 1000 * 1000) {
+    events.send(settingsToJson().c_str(), "settings", millis());
+    lastSettingsUpdateTime = state.lastLoopTimeStamp;
+    settingsUpdated = false;
+  }
+
+  state.rssiRaw = rssiRead();
+
+  state.rssiSmoothed = (state.filterRatioFloat * (float)state.rssiRaw) +
+                       ((1.0f - state.filterRatioFloat) * state.rssiSmoothed);
+  state.rssi = (int)state.rssiSmoothed;
+
+  // Measure peaks, only measure when in calibration mode.
+  if (state.calibrationMode && state.rssi > settings.rssiPeak) {
+    settings.rssiPeak = state.rssi;
+    updateRssiTrigger();
+    commitEeprom();
+  }
+  // Measure end.
+
+  if (state.lastLoopTimeStamp - lastRssiSendTime > rssiSendInterval) {
+    String rssiMsg = String(state.rssi) + " " + String(state.lastLoopTimeStamp);
+    // + " " + String(settings.rssiPeak)
+    // + " " + String(settings.enterRssiOffset)
+    // + " " + String(settings.leaveRssiOffset);
+#ifdef DEV_MODE
+    Serial.print("RSSI:");
+    Serial.println(rssiMsg);
+#endif
+    events.send(rssiMsg.c_str(), "rssi", state.lastLoopTimeStamp);
+
+    lastRssiSendTime = state.lastLoopTimeStamp;
+  }
+
+  if (!state.crossing &&
+      state.rssi > state.enterRssiTrigger
+      /**
+       * Make sure the next crossing only happens after MIN lap time.
+       *
+       * To avoid the following cases:
+       * 0. Last passed
+       * 1. Within MIN_LAP_TIME_MILLIS RSSI jump high, crossing again
+       * 2. What if that crossing rssi is too high, and no later RSSI passes
+       *    that?
+       *
+       * So what we should check here is the next crossing should not happen
+       * until MIN_LAP_TIME_MILLIS, instead of checking the leave.
+       *
+       * This also makes sure we don't get hang by an overly large crossing
+       * RSSI.
+       * */
+      && (state.lastLoopTimeStamp - lastPass.timeStamp * 1000 >
+          MIN_LAP_TIME_MICROS)) {
+    state.crossing = true; // Quad is going through the gate
+    Serial.println("Crossing = True");
+  }
+
+  if (state.crossing) {
+    state.rssiPeak = max(state.rssiPeak, state.rssi);
+
+    // Find the peak rssi and the time it occured during a crossing event
+    // Use the raw value to account for the delay in smoothing.
+    if (state.rssiRaw > state.rssiPeakRaw) {
+      state.rssiPeakRaw = state.rssiRaw;
+      state.rssiPeakRawTimeStamp = state.lastLoopTimeStamp / 1000;
+    }
+
+    uint16_t leaveRssiTrigger = state.leaveRssiTrigger;
+    if (state.calibrationMode) {
+      // Use lower trigger for leave when in calibration mode.
+      leaveRssiTrigger -=
+          (CALIBRATION_LEAVE_RSSI_FACTOR - 1) * settings.leaveRssiOffset;
+    }
+
+    // See if we have left the gate.
+    if (state.rssi < leaveRssiTrigger) {
+      uint32_t lastPassTimestamp = lastPass.timeStamp;
+
+      lastPass.rssiPeakRaw = state.rssiPeakRaw;
+      lastPass.rssiPeak = state.rssiPeak;
+      lastPass.timeStamp = state.rssiPeakRawTimeStamp;
+      lastPass.lap = lastPass.lap + 1;
+
+      String msg = String(lastPass.lap) + " " +
+                   String(lastPass.timeStamp - lastPassTimestamp) + " " +
+                   String(lastPass.rssiPeak)
+                   // The peak timestamp will be the initial timestamp of next
+                   // timing round.
+                   + " " + String(lastPass.timeStamp) + " " +
+                   String(state.lastLoopTimeStamp);
+
+      Serial.printf_P("Crossing = False >>>>>> %s\n", msg.c_str());
+      events.send(msg.c_str(), "newtime", state.lastLoopTimeStamp);
+
+      state.crossing = false;
+      state.rssiPeakRaw = 0;
+      state.rssiPeak = 0;
+
+      if (state.calibrationMode) {
+        state.calibrationPasses += 1;
+
+#ifdef DEV_MODE
+        Serial.println(">>>> Calibration pass");
+#endif
+
+        if (state.calibrationPasses >= CALIBRATION_PASSES &&
+            (state.lastLoopTimeStamp - state.calibrationStartMicros >
+             CALIBRATION_MIN_TIME_MICROS)) {
+          state.calibrationMode = false;
+          events.send("ended", "calibration", state.lastLoopTimeStamp);
+
+#ifdef DEV_MODE
+          Serial.println(">>>> Calibration done");
+#endif
+        }
+      }
+    }
+  }
+
+#if defined(ESP8266)
+  delay(8);
+#else
+  delay(1);
+#endif
+}
---
+++ src/fpvsim_timer.h
@@ -0,0 +1,234 @@
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+
+#define RSSI_PIN A0
+#else
+#include "WiFi.h"
+
+#define RSSI_PIN 34
+#endif
+
+
+#include <SPI.h>
+#include <EEPROM.h>
+#include <ESPAsyncWebServer.h>
+
+
+const int slaveSelectPin = SS; // Setup data pins for rx5808 comms
+const int spiDataPin = MOSI;
+const int spiClockPin = SCK;
+
+
+// #define DEV_MODE
+
+// Leave offset will multiply this factor when in calibration mode.
+uint16_t CALIBRATION_LEAVE_RSSI_FACTOR = 2;
+// Maximum passes for calibration to be considered done.
+uint8_t CALIBRATION_PASSES = 1;
+// Calibration has to last at least 60 seconds.
+uint32_t CALIBRATION_MIN_TIME_MICROS = 20 * 1000 * 1000;
+// Each lap has to take at least 4 seconds.
+uint32_t MIN_LAP_TIME_MICROS = 4 * 1000 * 1000;
+
+struct SettingsType {
+  uint16_t volatile vtxFreq = 5732;
+
+  uint8_t volatile filterRatio = 10;
+
+  // The RSSI when quad is close.
+  uint16_t volatile rssiPeak = 270;
+  uint16_t volatile enterRssiOffset = 5;
+  uint16_t volatile leaveRssiOffset = 20;
+
+  // Id of the timer, 0 - 25.
+  uint8_t volatile id = -1;
+
+  char apIp[20];
+  char localIp[20];
+  char apSsid[30];
+
+  // The router ssid and password.
+  char routerSsid[32];
+  char routerPwd[32];
+} settings;
+
+struct {
+  // True when the quad is going through the gate
+  bool volatile crossing = false;
+
+  // Current unsmoothed rssi
+  uint16_t volatile rssiRaw = 0;
+  // Smoothed rssi value, needs to be a float for smoothing to work
+  float volatile rssiSmoothed = 0;
+  // int representation of the smoothed rssi value
+  uint16_t volatile rssi = 0;
+
+  // Rssi has to be above the enter rssi to count as crossing.
+  uint16_t volatile enterRssiTrigger = 0;
+  // Rssi has to fall below the leave rssi to count as leaving.
+  uint16_t volatile leaveRssiTrigger = 0;
+
+  // The peak raw rssi seen the current pass
+  uint16_t volatile rssiPeakRaw = 0;
+  // The peak smoothed rssi seen the current pass
+  uint16_t volatile rssiPeak = 0;
+  // The time of the peak raw rssi for the current pass
+  uint32_t volatile rssiPeakRawTimeStamp = 0;
+
+  // variables to track the loop time
+  uint32_t volatile loopTime = 0;
+  uint32_t volatile lastLoopTimeStamp = 0;
+
+  // The float version of settings.filterRatio.
+  float volatile filterRatioFloat = 0.0f;
+
+  // The new vtx freq updated from user.
+  uint16_t volatile newVtxFreq = 5732;
+
+  // Whether in calibration mode right now, detecting peak rssi.
+  bool volatile calibrationMode = false;
+
+  uint32_t volatile calibrationStartMicros = 0;
+
+  // How many passes has done in calibration mode.
+  uint8_t volatile calibrationPasses = 0;
+
+  // Whether a client has connected.
+  bool volatile clientConnected = false;
+} state;
+
+struct {
+  uint16_t volatile rssiPeakRaw;
+  uint16_t volatile rssiPeak;
+  uint32_t volatile timeStamp;
+  uint8_t volatile lap;
+} lastPass;
+
+std::string settingsToJson() {
+  std::stringstream ss;
+  ss << "{" << std::endl;
+
+  ss << "\"vtxFreq\":" << settings.vtxFreq << "," << std::endl;
+  ss << "\"rssiPeak\":" << settings.rssiPeak << "," << std::endl;
+  ss << "\"enterRssiOffset\":" << settings.enterRssiOffset << "," << std::endl;
+  ss << "\"leaveRssiOffset\":" << settings.leaveRssiOffset << "," << std::endl;
+  ss << "\"apSsid\":\"" << settings.apSsid << "\"," << std::endl;
+  ss << "\"apIp\":\"" << settings.apIp << "\"," << std::endl;
+  ss << "\"localIp\":\"" << settings.localIp << "\"," << std::endl;
+
+  ss << "\"routerSsid\":\"" << settings.routerSsid << "\"," << std::endl;
+  ss << "\"routerPwd\":\"" << settings.routerPwd << "\"" << std::endl;
+
+  ss << "}";
+
+  return ss.str();
+}
+
+// Calculate rx5808 register hex value for given frequency in MHz
+uint16_t freqMhzToRegVal(uint16_t freqInMhz) {
+  uint16_t tf, N, A;
+  tf = (freqInMhz - 479) / 2;
+  N = tf / 32;
+  A = tf % 32;
+  return (N << 7) + A;
+}
+
+// Functions for the rx5808 module
+void SERIAL_SENDBIT1() {
+  digitalWrite(spiClockPin, LOW);
+  delayMicroseconds(300);
+  digitalWrite(spiDataPin, HIGH);
+  delayMicroseconds(300);
+  digitalWrite(spiClockPin, HIGH);
+  delayMicroseconds(300);
+  digitalWrite(spiClockPin, LOW);
+  delayMicroseconds(300);
+}
+void SERIAL_SENDBIT0() {
+  digitalWrite(spiClockPin, LOW);
+  delayMicroseconds(300);
+  digitalWrite(spiDataPin, LOW);
+  delayMicroseconds(300);
+  digitalWrite(spiClockPin, HIGH);
+  delayMicroseconds(300);
+  digitalWrite(spiClockPin, LOW);
+  delayMicroseconds(300);
+}
+void SERIAL_ENABLE_LOW() {
+  delayMicroseconds(100);
+  digitalWrite(slaveSelectPin, LOW);
+  delayMicroseconds(100);
+}
+void SERIAL_ENABLE_HIGH() {
+  delayMicroseconds(100);
+  digitalWrite(slaveSelectPin, HIGH);
+  delayMicroseconds(100);
+}
+
+// Set the frequency given on the rx5808 module
+void setRxModule(int frequency) {
+  Serial.print("Setup rx5808 frequency to: ");
+  Serial.println(frequency);
+
+  uint8_t i; // Used in the for loops
+
+  // Get the hex value to send to the rx module
+  uint16_t vtxHex = freqMhzToRegVal(frequency);
+
+  // bit bash out 25 bits of data / Order: A0-3, !R/W, D0-D19 / A0=0, A1=0,
+  // A2=0, A3=1, RW=0, D0-19=0
+  SERIAL_ENABLE_HIGH();
+  delay(2);
+  SERIAL_ENABLE_LOW();
+  SERIAL_SENDBIT0();
+  SERIAL_SENDBIT0();
+  SERIAL_SENDBIT0();
+  SERIAL_SENDBIT1();
+  SERIAL_SENDBIT0();
+
+  for (i = 20; i > 0; i--)
+    SERIAL_SENDBIT0(); // Remaining zeros
+
+  SERIAL_ENABLE_HIGH(); // Clock the data in
+  delay(2);
+  SERIAL_ENABLE_LOW();
+
+  // Second is the channel data from the lookup table, 20 bytes of register data
+  // are sent, but the MSB 4 bits are zeros register address = 0x1, write,
+  // data0-15=vtxHex data15-19=0x0
+  SERIAL_ENABLE_HIGH();
+  SERIAL_ENABLE_LOW();
+
+  SERIAL_SENDBIT1(); // Register 0x1
+  SERIAL_SENDBIT0();
+  SERIAL_SENDBIT0();
+  SERIAL_SENDBIT0();
+
+  SERIAL_SENDBIT1(); // Write to register
+
+  // D0-D15, note: loop runs backwards as more efficent on AVR
+  for (i = 16; i > 0; i--) {
+    if (vtxHex & 0x1) { // Is bit high or low?
+      SERIAL_SENDBIT1();
+    } else {
+      SERIAL_SENDBIT0();
+    }
+    vtxHex >>= 1; // Shift bits along to check the next one
+  }
+
+  for (i = 4; i > 0; i--) // Remaining D16-D19
+    SERIAL_SENDBIT0();
+
+  SERIAL_ENABLE_HIGH(); // Finished clocking data in
+  delay(2);
+
+  digitalWrite(slaveSelectPin, LOW);
+  digitalWrite(spiClockPin, LOW);
+  digitalWrite(spiDataPin, LOW);
+
+  settings.vtxFreq = frequency;
+  Serial.println("rx5808 set.");
+}
+
+// Read the RSSI value for the current channel
+int rssiRead() { return analogRead(RSSI_PIN); }
